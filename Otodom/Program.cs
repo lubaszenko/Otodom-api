376279612a831bef451614e4eca7b39985f10943
michu <michu.ad@wp.pl>
2024-01-19 15:27:29 +0100

    Przekierowanie swaggera

--- Otodom/Program.cs
+++ Otodom/Program.cs
@@ -24,6 +24,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
